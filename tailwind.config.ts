b6723a86d07056cb8243174fb446de68448b9a32
mayinuddin <mayinuddin.munna@gmail.com>
2025-01-25 20:28:26 +0600

    tailwind init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,13 @@
-import type { Config } from "tailwindcss";
+import { heroui } from "@heroui/theme";
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 };
-export default config;
