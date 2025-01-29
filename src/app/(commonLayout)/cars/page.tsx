d6c048f52582deb567b3187c438450917d9ed240
mayinuddin <mayinuddin.munna@gmail.com>
2025-01-29 22:27:40 +0600

    push

--- src/app/(commonLayout)/cars/page.tsx
+++ src/app/(commonLayout)/cars/page.tsx
@@ -1,27 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import nexiosInstance from "@/app/config/nexios.config";
 import { Card, CardFooter, CardHeader } from "@heroui/card";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Cars = async () => {
-  const res = await fetch("http://localhost:5000/api/v1/cars", {
-    next: {},
+  const { data }: any = await nexiosInstance.get("/cars", {
     cache: "no-store",
+    next: {},
   });
-
-  const posts = await res.json();
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-800 dark:to-gray-900">
-      {/* Page Title */}
       <p className="text-4xl font-bold text-gray-800 dark:text-gray-100 mt-6 mb-14 tracking-wider">
         All Cars
       </p>
 
       {/* Cards Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-6 w-full max-w-7xl">
-        {posts?.data?.map((item: any) => (
+        {data?.data?.map((item: any) => (
           <Card
             key={item._id}
             className="relative w-full h-[350px] rounded-xl overflow-hidden shadow-lg transform transition-transform hover:scale-105 hover:shadow-2xl hover:z-20"
@@ -37,14 +35,13 @@ const Cars = async () => {
             </CardHeader>
 
             {/* Car Image */}
-            {/* <Image
-              // removeWrapper
+            <Image
               alt={item?.name}
               className="z-0 w-full h-full object-cover transition-transform duration-500"
               src={item?.image}
               height={300}
               width={300}
-            /> */}
+            />
 
             {/* Card Footer */}
             <CardFooter className="absolute bottom-0 left-0 right-0 p-4 backdrop-blur-lg bg-white/20 dark:bg-black/20 border-t border-gray-200 dark:border-gray-600 flex justify-between items-center">
