615c6f558a0d7f1de221cf12d38fdc6f9c848deb
mayinuddin <mayinuddin.munna@gmail.com>
2025-02-01 14:53:26 +0600

    google login user info stored in DB

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import nexiosInstance from "@/app/config/nexios.config";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { cookies } from "next/headers";
 
 const handler = NextAuth({
   providers: [
@@ -8,6 +11,36 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async signIn({ profile, account }: any) {
+      try {
+        if (!account || !profile) {
+          return false;
+        }
+
+        if (account?.provider === "google") {
+          const res: any = await nexiosInstance.post("/auth/login", {
+            name: profile.name,
+            email: profile.email,
+            img: profile?.picture,
+          });
+
+          if (res.data.data.accessToken || res.data.data.refreshToken) {
+            cookies().set("accessToken", res.data.data.accessToken);
+            cookies().set("refreshToken", res.data.data.refreshToken);
+            return true;
+          } else {
+            return false;
+          }
+        } else {
+          return false;
+        }
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
   pages: {
     signIn: "login",
   },
